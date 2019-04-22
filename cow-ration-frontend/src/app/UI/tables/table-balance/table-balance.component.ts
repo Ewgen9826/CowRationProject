22aee80829df2182ea7a387fe7f9b59562a5151c
0aleks07 <aleksyu.krasyuk@mail.ru>
2019-04-22 20:15:42 +0300

    *table-balance.components скрол по кнопкам

--- cow-ration-frontend/src/app/UI/tables/table-balance/table-balance.component.ts
+++ cow-ration-frontend/src/app/UI/tables/table-balance/table-balance.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation  } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, ViewChild, ElementRef  } from '@angular/core';
 
 @Component({
   selector: 'app-table-balance',
@@ -7,12 +7,30 @@ import { Component, OnInit, Input, ViewEncapsulation  } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class TableBalanceComponent implements OnInit {
+  intervalBotton;
+  intervalTop;
 
   @Input() headerFirstBalance: string;
   @Input() headerSecondBalance: string;
-  
-  
+  @ViewChild('widgetsContent', { read: ElementRef }) public widgetsContent: ElementRef<any>;
+  public scrollTop(): void {
+    this.intervalTop = 200;
+    this.widgetsContent.nativeElement.scrollTo({ top: (this.widgetsContent.nativeElement.scrollTop - this.intervalTop), behavior: 'smooth' });
+    this.intervalBotton = 0;
+  }
+  public scrollBotton(): void {      
+    this.intervalBotton = 200;
+    this.widgetsContent.nativeElement.scrollTo({ top: (this.widgetsContent.nativeElement.scrollTop + this.intervalBotton), behavior: 'smooth' });
+    this.intervalTop = 0;    
+  }
+
   elements: any = [
+    {firstColumn: 'Солома ячменная', secondColumn: '85'},
+    {firstColumn: 'Сенаж злаковый', secondColumn: '837' },
+    {firstColumn: 'Силож кукурузный', secondColumn: '8874' },
+    {firstColumn: 'Солома ячменная', secondColumn: '85'},
+    {firstColumn: 'Сенаж злаковый', secondColumn: '837' },
+    {firstColumn: 'Силож кукурузный', secondColumn: '8874' },
     {firstColumn: 'Солома ячменная', secondColumn: '85'},
     {firstColumn: 'Сенаж злаковый', secondColumn: '837' },
     {firstColumn: 'Силож кукурузный', secondColumn: '8874' },
