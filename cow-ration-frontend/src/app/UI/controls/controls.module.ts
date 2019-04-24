a18edc89f11cf5cc34f37b9e04d51f793b50650d
Ewgeny <kuzmich-ewgen@list.ru>
2019-04-24 14:32:30 +0300

    * fixed probleb with import

--- cow-ration-frontend/src/app/UI/controls/controls.module.ts
+++ cow-ration-frontend/src/app/UI/controls/controls.module.ts
@@ -1,42 +1,40 @@
-import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { ButtonCloseComponent } from './button-close/button-close.component';
-import { ButtonChoiceComponent } from './button-choice/button-choice.component';
-import { ButtonSortingComponent } from './button-sorting/button-sorting.component';
-import { SquareBlockComponent } from './square-block/square-block.component';
-import { CounterComponent } from './counter/counter.component';
-import { InitialParametersBlockComponent } from './initial-parameters-block/initial-parameters-block.component';
-import { ButtonComponent } from './button/button.component';
-import { RouterModule } from '@angular/router';
-import { TextboxComponent } from './textbox/textbox.component';
-import { VerticalSliderComponent } from './vertical-slider/vertical-slider.component';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { ButtonCloseComponent } from "./button-close/button-close.component";
+import { ButtonChoiceComponent } from "./button-choice/button-choice.component";
+import { ButtonSortingComponent } from "./button-sorting/button-sorting.component";
+import { SquareBlockComponent } from "./square-block/square-block.component";
+import { CounterComponent } from "./counter/counter.component";
+import { InitialParametersBlockComponent } from "./initial-parameters-block/initial-parameters-block.component";
+import { ButtonComponent } from "./button/button.component";
+import { RouterModule } from "@angular/router";
+import { TextboxComponent } from "./textbox/textbox.component";
+import { VerticalSliderComponent } from "./vertical-slider/vertical-slider.component";
 
 @NgModule({
-  declarations: 
-  [
+  declarations: [
     ButtonCloseComponent,
     ButtonChoiceComponent,
-    ButtonSortingComponent, 
-    SquareBlockComponent, 
-    CounterComponent, 
-    InitialParametersBlockComponent, 
-    ButtonComponent, TextboxComponent, VerticalSliderComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule
-  ],
-  exports: 
-  [
-    ButtonCloseComponent,
-    ButtonChoiceComponent, 
-    ButtonSortingComponent, 
-    SquareBlockComponent, 
+    ButtonSortingComponent,
+    SquareBlockComponent,
     CounterComponent,
     InitialParametersBlockComponent,
-    ButtonComponent
+    ButtonComponent,
+    TextboxComponent,
+    VerticalSliderComponent
+  ],
+  imports: [CommonModule, FormsModule, RouterModule],
+  exports: [
+    ButtonCloseComponent,
+    ButtonChoiceComponent,
+    ButtonSortingComponent,
+    SquareBlockComponent,
+    CounterComponent,
+    InitialParametersBlockComponent,
+    ButtonComponent,
+    VerticalSliderComponent,
+    TextboxComponent
   ]
 })
-export class ControlsModule { }
+export class ControlsModule {}
--- cow-ration-frontend/src/app/UI/milk-settings-card/milk-settings-card.module.ts
+++ cow-ration-frontend/src/app/UI/milk-settings-card/milk-settings-card.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MilkSettingsCardComponent } from "./milk-settings-card.component";
-import { VerticalSliderComponent } from "../controls/vertical-slider/vertical-slider.component";
-import { TextboxComponent } from "../controls/textbox/textbox.component";
+import { ControlsModule } from "../controls/controls.module";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ControlsModule],
   declarations: [
-    MilkSettingsCardComponent,
-    VerticalSliderComponent,
-    TextboxComponent
+    MilkSettingsCardComponent
   ],
   exports: [MilkSettingsCardComponent]
 })
